133505204c2e46f6d53224ac390d6c4a0b5fc06e
Dave Calleja <callejad@tipico.com>
2021-09-09 11:01:14 +0200

    Removing change

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={{ backgroundColor }}
       {...props}
     >
-      {label} test
+      {label}
     </button>
   );
 };
